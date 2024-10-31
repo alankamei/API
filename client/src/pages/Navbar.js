8a640a1a4b061545e896a82cc583a8177a8e86c2
Alan Kamei <gaipuilankmei@gmail.com>
2024-10-31 20:59:17 +0530

    added protected home for user who logged in

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useRouteError } from 'react-router-dom';
+
+import { Link } from 'react-router-dom';
 import { useUser } from '../UserContext';
 
 function Navbar() {
